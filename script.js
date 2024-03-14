d19666107da6540c336a20e3a886c03a23f06eda
Ritesh Kishor Chaudhari <100477678+Ritu0097@users.noreply.github.com>
2024-03-14 23:12:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,21 +6,28 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
-btn.addEventListener("click", async () => {
-  try {
-    const imagePromises = images.map((image) => {
-      return new Promise((resolve, reject) => {
-        const img = new Image();
-        img.src = image.url;
-        img.onload = () => resolve(img);
-        img.onerror = () => reject(new Error(`Failed to load image's URL: ${image.url}`));
-      });
-    });
-    const downloadedImages = await Promise.all(imagePromises);
-    downloadedImages.forEach((img) => {
-      output.appendChild(img);
-    });
-  } catch (error) {
-    console.error(error);
-  }
-});
+function loadImage(url) {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.src = url;
+
+    console.dir(img);
+    img.onload = () => resolve(img);
+    img.onerror = () => reject(`Failed to load image's URL: ${url}`);
+  });
+}
+
+function downloadImages(imageArray) {
+  return Promise.all(imageArray.map((imageObj) => loadImage(imageObj.url)));
+}
+
+function displayImages(images) {
+  images.forEach((img) => output.appendChild(img));
+}
+
+document
+  btn.addEventListener("click", () => {
+    downloadImages(images)
+      .then(displayImages)
+      .catch((error) => console.error(error));
+  });
